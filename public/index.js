67d020ec56874a026753ff71b716adfdc52b3551
Juliette Tripier <juli.tripier@gmail.com>
2024-01-07 17:39:12 -0500

    Testing event key

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 console.log('Hello world');
 
 document.addEventListener('keydown', function(event) {
+    console.log(event);
     if (event.key == 38) {
         console.log('Up arrow pressed');
     } 
