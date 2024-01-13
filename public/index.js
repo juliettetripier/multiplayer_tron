e879f680412dc736333aecd69f98c6ee9e4e4f29
Juliette Tripier <juli.tripier@gmail.com>
2024-01-13 14:37:18 -0500

    Added code to send message to server on up arrow down

--- public/index.js
+++ public/index.js
@@ -2,8 +2,11 @@ console.log('Hello world');
 
 const ws = new WebSocket('wss://poised-brook-chartreuse.glitch.me:3000/wss')
 
+// ws.addEventListener('message', )
+
 document.addEventListener('keydown', function(event) {
     if (event.key == 'ArrowUp') {
         console.log('Up arrow pressed');
+        ws.send('message');
     } 
 })
