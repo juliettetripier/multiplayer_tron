8a7a719e953d05e89ba4644b09cc331997577bf0
Juliette Tripier <juli.tripier@gmail.com>
2024-01-13 15:16:42 -0500

    Added Client class to index.js

--- public/index.js
+++ public/index.js
@@ -1,16 +1,26 @@
 console.log('Hello world');
 
+class Client {
+    constructor() {
+        const ui = document.querySelector('#ui');
+
+        ws.addEventListener('message', (event) => {
+            ui.innerText = event.data;
+        })
+
+        document.addEventListener('keydown', function(event) {
+            if (event.key == 'ArrowUp') {
+                console.log('Up arrow pressed');
+                ws.send('message');
+            } 
+        })
+    }
+
+
+}
+
 const ws = new WebSocket('wss://poised-brook-chartreuse.glitch.me/wss');
 
-const ui = document.querySelector('#ui');
-
-ws.addEventListener('message', (event) => {
-    ui.innerText = event.data;
-})
-
-document.addEventListener('keydown', function(event) {
-    if (event.key == 'ArrowUp') {
-        console.log('Up arrow pressed');
-        ws.send('message');
-    } 
+window.addEventListener('DOMContentLoaded', () => {
+    const new_client = new Client()
 })
