bca3b98d1b4bbddea28e860eae0c254c8eeef076
Juliette Tripier <juli.tripier@gmail.com>
2024-01-20 16:07:56 -0500

    Filled in canvas with background color in Local class

--- public/index.js
+++ public/index.js
@@ -19,8 +19,26 @@ class Client {
 
 }
 
+class Local {
+    constructor() {
+        this.initializeGame();
+    }
+
+    initializeGame() {
+        const canvas = document.querySelector('#main-canvas');
+        const ctx = canvas.getContext('2d');
+
+        ctx.beginPath();
+        ctx.rect(0,0,400,400);
+        ctx.fillStyle = 'indigo';
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
 const ws = new WebSocket('wss://poised-brook-chartreuse.glitch.me/wss');
 
 window.addEventListener('DOMContentLoaded', () => {
-    const new_client = new Client()
+    const new_client = new Client();
+    const new_local = new Local();
 })
