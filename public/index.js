b4b1238f84b98b586b88a0cc1d46e16b7a56f40e
Juliette Tripier <juli.tripier@gmail.com>
2024-01-27 15:27:35 -0500

    Added update method

--- public/index.js
+++ public/index.js
@@ -21,16 +21,63 @@ class Client {
 
 class Local {
     constructor() {
+        this.entities = [];
         this.initializeGame();
     }
 
     initializeGame() {
-        const canvas = document.querySelector('#main-canvas');
-        const ctx = canvas.getContext('2d');
+        this.canvas = document.querySelector('#main-canvas');
+        this.ctx = this.canvas.getContext('2d');
+        const background = new Background();
+        this.addEntity(background);
+        const player = new Player();
+        this.addEntity(player);
+        this.tick();
+    }
 
+    addEntity(entity) {
+        this.entities.push(entity);
+    }
+
+    tick() {
+        this.ctx.clearRect(0, 0, 400, 400);
+        this.entities.forEach((entity) => entity.update())
+        this.entities.forEach((entity) => entity.draw(this.ctx));
+    }
+}
+
+class Background {
+    draw(ctx) {
         ctx.beginPath();
         ctx.rect(0,0,400,400);
         ctx.fillStyle = 'indigo';
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    update() {}
+}
+
+class Player {
+    constructor() {
+        this.playerLocation = {
+            x: 20,
+            y: 20
+        };
+        this.turnHistory = [];
+        this.lastUpdated = null;
+    }
+
+    update() {
+        console.log(this.lastUpdated);
+        this.lastUpdated = Date.now();
+        console.log(this.lastUpdated);
+    }
+
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.playerLocation.x, this.playerLocation.y, 10, 0, Math.PI * 2);
+        ctx.fillStyle = 'yellow';
         ctx.fill();
         ctx.closePath();
     }
